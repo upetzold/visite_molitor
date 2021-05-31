ed8dd8eeba8049e09852eebe34ee7fd19f3bc6a6
Ulrike Petzold <ulrike.petzold@gmail.com>
2021-05-31 21:11:24 +0200

    visite virtuelle

---
+++ app-files/data.js
@@ -0,0 +1,374 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-accueil",
+      "name": "accueil",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1936,
+      "initialViewParameters": {
+        "yaw": -1.6698271308865422,
+        "pitch": -0.014991980830968643,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.23162152087322774,
+          "pitch": 0.23930969263152058,
+          "rotation": 0,
+          "target": "1-espace-bleu1"
+        },
+        {
+          "yaw": -3.067035359037792,
+          "pitch": 0.10644760333837766,
+          "rotation": 0,
+          "target": "4-espace-vert"
+        },
+        {
+          "yaw": -3.06371433052397,
+          "pitch": 0.2529748957940665,
+          "rotation": 0.7853981633974483,
+          "target": "3-espace-langues"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.0425408850411735,
+          "pitch": 0.4917759147978522,
+          "title": "Bureau d'accueil<br>",
+          "text": "Pour vous renseigner, enregistrer vos prêts et retours"
+        },
+        {
+          "yaw": 1.871235426111344,
+          "pitch": -0.005621992811613907,
+          "title": "Revues<br>",
+          "text": "Les revues en sciences de l’éducation, pratiques de classes, généralités"
+        },
+        {
+          "yaw": 3.0295082180452937,
+          "pitch": 0.19285441793190827,
+          "title": "Copieur<br>",
+          "text": "Pour vos photocopies (carte Izly ou cartadis)"
+        }
+      ]
+    },
+    {
+      "id": "1-espace-bleu1",
+      "name": "espace bleu1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1936,
+      "initialViewParameters": {
+        "yaw": -0.011947578378102008,
+        "pitch": 0.011243985623227815,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12195913768734457,
+          "pitch": 0.19199879706000544,
+          "rotation": 0,
+          "target": "2-espace-bleu2"
+        },
+        {
+          "yaw": 3.0810064493170835,
+          "pitch": 0.3365631499983319,
+          "rotation": 0,
+          "target": "0-accueil"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.850141950469487,
+          "pitch": 0.15471921832103952,
+          "title": "Postes informatiques<br>",
+          "text": "Accès à internet et bureautique"
+        },
+        {
+          "yaw": -1.7464100047488902,
+          "pitch": 0.29316096091815425,
+          "title": "Arts visuels<br>",
+          "text": "Livres, DVD, affiches sur les arts visuels et leur didactique<br>"
+        },
+        {
+          "yaw": 0.42243711279105334,
+          "pitch": 0.22185397580561883,
+          "title": "Littérature jeunesse<br>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.6575590351256455,
+          "pitch": 0.24331632158072836,
+          "title": "Psychologie",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "2-espace-bleu2",
+      "name": "espace bleu2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1936,
+      "initialViewParameters": {
+        "yaw": 0.19601372606171275,
+        "pitch": -0.02998396166194084,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9784240130347452,
+          "pitch": 0.3022571997499277,
+          "rotation": 0,
+          "target": "0-accueil"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.18146665614825963,
+          "pitch": 0.39506534183432507,
+          "title": "Annales",
+          "text": "Documents pour préparer les concours<br>"
+        },
+        {
+          "yaw": 1.9810455754684257,
+          "pitch": 0.1729352584732169,
+          "title": "Sciences",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.01630732306612437,
+          "pitch": -0.03033556542364302,
+          "title": "Français",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.5725217642776119,
+          "pitch": 0.005627693137142842,
+          "title": "Mathématiques",
+          "text": "<br>"
+        },
+        {
+          "yaw": -1.8102158457169715,
+          "pitch": 0.12343821812964961,
+          "title": "Sciences de l'éducation<br>",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "3-espace-langues",
+      "name": "espace langues",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1936,
+      "initialViewParameters": {
+        "yaw": 0.9172267081081635,
+        "pitch": 0.12492419000605182,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8091121068449816,
+          "pitch": 0.2827123359871102,
+          "rotation": 11.780972450961727,
+          "target": "0-accueil"
+        },
+        {
+          "yaw": -0.8852543993627933,
+          "pitch": 0.4110948571534081,
+          "rotation": 7.853981633974483,
+          "target": "4-espace-vert"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.292904948617533,
+          "pitch": 0.027164344383116656,
+          "title": "Séries",
+          "text": "en anglais et en allemand<br>"
+        },
+        {
+          "yaw": 1.3823291369993438,
+          "pitch": -0.08350080595610976,
+          "title": "Revues",
+          "text": "en langue étrangère<br>"
+        },
+        {
+          "yaw": 0.986707436061872,
+          "pitch": 0.4685354495297638,
+          "title": "Postes informatiques<br>",
+          "text": "bureautique, enregistrements audio, Internet<br>"
+        },
+        {
+          "yaw": -1.4079810150311438,
+          "pitch": 0.3286596561569386,
+          "title": "Conseils personnalisés sur l'apprentissage des langues<br>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 1.9047092508445083,
+          "pitch": 0.028990516193182714,
+          "title": "Visionnage de vidéos<br>",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "4-espace-vert",
+      "name": "espace vert",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1936,
+      "initialViewParameters": {
+        "yaw": -0.8647435518743976,
+        "pitch": 0.09560350621273805,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.031318687032707,
+          "pitch": 0.340318289594304,
+          "rotation": 6.283185307179586,
+          "target": "0-accueil"
+        },
+        {
+          "yaw": 1.8793425785597933,
+          "pitch": 0.341625201698335,
+          "rotation": 0.7853981633974483,
+          "target": "3-espace-langues"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8049717499824673,
+          "pitch": 0.1026381908042957,
+          "title": "Cinéma",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.9457821747174595,
+          "pitch": 0.2259864175377828,
+          "title": "Langues vivantes<br>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.5004404125235897,
+          "pitch": 0.05101659496632038,
+          "title": "Histoire",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.17821774720282768,
+          "pitch": 0.05122783572000955,
+          "title": "Musique",
+          "text": "<br>"
+        },
+        {
+          "yaw": 3.1408692257264734,
+          "pitch": 0.06561313643548061,
+          "title": "Postes vidéo<br>",
+          "text": "<p class=\"MsoNormal\" style=\"margin: 0px; line-height: normal;\">Lecture des DVD et VHS</p>"
+        },
+        {
+          "yaw": 0.1421413715782478,
+          "pitch": 0.3722414605343758,
+          "title": "Postes informatiques<br>",
+          "text": "Accès à internet et bureautique"
+        }
+      ]
+    }
+  ],
+  "name": "Visite médiathèque Molitor",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
